922905c6c40d27ecf1e913d3ede784adf863dd7a
Aman-vijay <shivamvj04@gmail.com>
2025-03-01 20:14:48 +0530

    add routing and layout structure with Header and Footer components; update title and dependencies

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,14 +1,16 @@
+import { Outlet } from 'react-router';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   )
 }
 
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {ErrorPage} from './pages/ErrorPage.tsx'
+import Login from './pages/Login.tsx'
+import Dashboard from './pages/Dashboard.tsx'
+import Home from './pages/Home.tsx'
+
+import { Route, createBrowserRouter, createRoutesFromElements,RouterProvider } from 'react-router'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<ErrorPage/>}>
+    <Route index element={<Home/>} />  
+    <Route path="/login" element={<Login/>} />
+    <Route path="/dashboard" element={<Dashboard/>} />
+    
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
