d6784dfe8e1b72a26734290da3921d864e49e286
Aman-vijay <shivamvj04@gmail.com>
2025-03-04 13:48:33 +0530

    feat(frontend): Update main app configuration and routing

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,27 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import {ErrorPage} from './pages/ErrorPage.tsx'
-import Login from './pages/Login.tsx'
-import Dashboard from './pages/Dashboard.tsx'
-import Home from './pages/Home.tsx'
-
-import { Route, createBrowserRouter, createRoutesFromElements,RouterProvider } from 'react-router'
+// src/Main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { ErrorPage } from './pages/ErrorPage.tsx';
+import Login from './pages/Login.tsx';
+import Dashboard from './pages/Dashboard.tsx';
+import Home from './pages/Home.tsx';
+import GitHubCallback from './pages/GitHubCallback.tsx';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />} errorElement={<ErrorPage/>}>
-    <Route index element={<Home/>} />  
-    <Route path="/login" element={<Login/>} />
-    <Route path="/dashboard" element={<Dashboard/>} />
-    
+    <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+      <Route index element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/integrations/github/oauth2/callback" element={<GitHubCallback />} />
     </Route>
   )
-)
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
